7ee684eb9469475698abd51f1259096073fe6925
FranBudde <buddemeyerizzo@gmail.com>
2025-05-03 14:44:39 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+// require('dotenv').config();
+
+import express from "express";
+import mainRoutes from "./routes/main.js"
+
+const app = express();
+
+/* Routers */
+
+app.use(express.urlencoded({ extended: false })); // esto nos permite capturar la informacion que se envia por un formulario via POST (req.body)
+app.use(express.json());
+
+
+app.use('/', mainRoutes)
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => {
+    console.log(`Levantando un servidor con Express en: http://localhost:${port}`);
+});
