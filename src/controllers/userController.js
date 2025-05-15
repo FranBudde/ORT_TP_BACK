3f44d586deb3678f62bf9126de4b1109ebfcd013
Santiago Khazki <santiagokhazki@gmail.com>
2025-05-15 19:57:08 -0300

    emprolijo codigo

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 import userService from "../service/userService.js";
 import jwt from "jsonwebtoken";
 
-const controller = {
+const userController = {
   getUsers: async (req, res) => {
     const users = await userService.getUsers();
 
@@ -9,7 +9,7 @@ const controller = {
       .status(200)
       .json({ message: "Users retrieved successfully", data: users });
   },
-  userAuth: async (req, res) => {
+  login: async (req, res) => {
     const { username, password } = req.body;
     let response;
 
@@ -18,13 +18,13 @@ const controller = {
         .status(400)
         .json({ message: "User or Password cannot be empty" });
     } else {
-      const user = await userService.userAuth(username, password);
+      const user = await userService.login(username, password);
       if (user) {
         const token = jwt.sign(
           { userId: user._id, username: user.userName },
           process.env.JWT_SECRET,
           { expiresIn: "1h" }
-        ); // Guarda tu clave secreta en una variable de entorno
+        );
         response = res
           .status(200)
           .json({ message: "User retrieved successfully", token: token }); // Usuario encontrado, devuelve la información con un 200 OK
@@ -41,7 +41,7 @@ const controller = {
     const newUser = req.body;
     const username = newUser["userName"]
 
-    //Cheque si existe el usuario
+    //Chequeo si existe el usuario
     const check_user_exists = await userService.check_user_exists(username);
 
     let response;
@@ -73,4 +73,4 @@ const controller = {
   },
 };
 
-export default controller;
+export default userController;
--- src/service/userService.js
+++ src/service/userService.js
@@ -1,62 +1,56 @@
+import { getDB } from "../data/connections.js";
+import { ObjectId } from "mongodb";
 
-import { getDB } from '../data/connections.js';
-import { ObjectId } from 'mongodb';
-
-const usersCollection = process.env.DB_USERS_COLLECTION
+const usersCollection = process.env.DB_USERS_COLLECTION;
 
 export async function getUsers() {
-    const db = await getDB();
- 
-    const users = db
-        .collection(`${usersCollection}`)
-        .find()
-        .toArray();
+  const db = await getDB();
 
-    return users;
+  const users = db.collection(`${usersCollection}`).find().toArray();
+
+  return users;
 }
 
-export async function userAuth(username, password) {
-    const db = await getDB();
-
-    const user = db
-      .collection(`${usersCollection}`)
-      .findOne({ userName: username, password: password });
-
-    return user;
-  }
-
-export async function check_user_exists(username){
+export async function login(username, password) {
   const db = await getDB();
 
   const user = db
     .collection(`${usersCollection}`)
-    .findOne({ userName: username});
+    .findOne({ userName: username, password: password });
 
   return user;
 }
 
-export async function insert_user(newUser){
+export async function check_user_exists(username) {
   const db = await getDB();
 
-  const users_collection = db.collection(`${usersCollection}`)
+  const user = db
+    .collection(`${usersCollection}`)
+    .findOne({ userName: username });
 
-  try{
+  return user;
+}
+
+export async function insert_user(newUser) {
+  const db = await getDB();
+
+  const users_collection = db.collection(`${usersCollection}`);
+
+  try {
     
     const result = await users_collection.insertOne(newUser);
-    console.log("respuesta insert: ", result)
-    return result
-
-  } catch(error){
-
-    console.log("Error al insertar el usuario: ", error)
-    throw(error)
-
+    
+    return result;
+  }
+  catch (error) {
+    
+    throw error;
   }
 }
 
 export default {
-    getUsers,
-    userAuth,
-    check_user_exists,
-    insert_user
+  getUsers,
+  login,
+  check_user_exists,
+  insert_user,
 };
