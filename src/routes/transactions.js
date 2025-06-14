4bf064ef0e400baf65d73a2000e33fe94a2dce4d
Santiago Khazki <santiagokhazki@gmail.com>
2025-06-14 20:21:24 -0300

    get trx by user, date and type

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -5,6 +5,7 @@ import { verifyToken } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.get('/get_transaccions',transactionsController.getTransaccions); 
+router.post('/get_transaction_by_user', transactionsController.getTransactionsByUser);
 router.post('/create_transaccion',verifyToken,transactionsController.createTransaccion);
 router.post('/get_total_balance', transactionsController.getTotalBalance)
 router.post('/update_balance', transactionsController.updateBalance);
