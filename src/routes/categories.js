c5f64de529c676d98d0de149af49f70693630694
Santiago Khazki <santiagokhazki@gmail.com>
2025-06-25 19:45:11 -0300

    deje un comentario

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -2,7 +2,7 @@ import express from "express";
 import { handleGetCategoryByName } from "../controllers/categoryController.js";
 
 const router = express.Router();
-
+// Esto es un comentario
 router.get("/get_by_name/:name", handleGetCategoryByName);
 
 export default router;
